18cad32404f75af818cb59df5eb3f732a8973fab
Bluetouch <bogusz28@wp.pl>
2020-09-13 10:51:02 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'directives-pipes-app';
+  public loremValue = 'lorem ipsum';
+  public example = 'LOREM IPSUM';
+  public jsonObjec = {
+    properties: 'lorem'
+  };
+  public birthday = Date.now();
+  public isAuthenticated = false;
+  public persons: string[] = ['Adam Kowalski', 'Marcin Janiak', 'Rober Lewandowski'];
+  public lorem = 'lorem-ipsum-blabla';
+
 }
